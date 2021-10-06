be07ac8181f390614df7d19f38ea42f44396f81a
Arkadiusz Milewski <asmilewski@gmail.com>
2021-10-06 17:00:04 +0200

    fixing warning

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,49 +3,9 @@ import React from "react";
 const Main = () => {
   return (
     <div className="ui text container">
-      <div className="ui divided three column grid">
-        <div className="row">
-          <div className="column">Image</div>
-          <div className="column">Descripion</div>
-          <div className="column">Descripion</div>
-        </div>
-        <div className="row">
-          <div className="column">Image</div>
-          <div className="column">Descripion</div>
-          <div className="column">Descripion</div>
-        </div>
-        <div className="row">
-          <div className="column">Image</div>
-          <div className="column">Descripion</div>
-          <div className="column">Descripion</div>
-        </div>
-      </div>
+      <h1>Please, Enjoy a selection of amazing products in our Shop.</h1>
     </div>
   );
 };
-<div class="ui divided three column grid">
-  <div class="row">
-    <div class="column">
-      <img src="/images/wireframe/media-paragraph.png" class="ui image" />
-    </div>
-    <div class="column">
-      <img src="/images/wireframe/media-paragraph.png" class="ui image" />
-    </div>
-    <div class="column">
-      <img src="/images/wireframe/media-paragraph.png" class="ui image" />
-    </div>
-  </div>
-  <div class="row">
-    <div class="column">
-      <img src="/images/wireframe/media-paragraph.png" class="ui image" />
-    </div>
-    <div class="column">
-      <img src="/images/wireframe/media-paragraph.png" class="ui image" />
-    </div>
-    <div class="column">
-      <img src="/images/wireframe/media-paragraph.png" class="ui image" />
-    </div>
-  </div>
-</div>;
 
 export default Main;
---
+++ src/components/Shop.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+const Shop = () => {
+  return (
+    <div className="main ui container">
+      <div className="ui divided three column grid">
+        <div className="row">
+          <div className="column">Image</div>
+          <div className="column">Descripion</div>
+          <div className="column">Descripion</div>
+        </div>
+        <div className="row">
+          <div className="column">Image</div>
+          <div className="column">Descripion</div>
+          <div className="column">Descripion</div>
+        </div>
+        <div className="row">
+          <div className="column">Image</div>
+          <div className="column">Descripion</div>
+          <div className="column">Descripion</div>
+        </div>
+      </div>
+    </div>
+  );
+};
